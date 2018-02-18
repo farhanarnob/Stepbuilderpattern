8a071eff828b6f2bbeb7207727d2ebfa5631b0e5
Farhan Rahman Arnob <farhanarnob@live.com>
2018-02-19 02:29:34 +0600

    Implement Step Builder pattern using lambda expression

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,12 @@ import model.UserBuilder;
 
 public class Main {
     public static void main(String[] args) {
-        User user = UserBuilder.newBuilder()
+        User user = UserBuilder
+                .newBuilder()
                 .setName("Arnob")
-                .setAge("")
-                .setWeight("")
-                .setHeight("")
+                .setAge("23")
+                .setHeight("233")
+                .setWeight("234234")
                 .build();
 //        Mandatory user Name Field. So this will not run.
 //        User user1 = UserBuilder.newBuilder()
--- src/model/UserBuilder.java
+++ src/model/UserBuilder.java
@@ -6,73 +6,30 @@ public class UserBuilder {
     }
 
     public static SetName newBuilder() {
-        return new Builder();
+        return new Builder().getSetName();
     }
-
-
     public interface SetName {
         SetAge setName(String name);
     }
-
-    public interface SetAge extends SetHeight {
-        Extra setAge(String age);
+    public interface SetAge {
+        SetHeight setAge(String age);
     }
-
     public interface SetHeight {
-        ExtraWeightWithBuild setHeight(String height);
+        SetWeight setHeight(String height);
 
     }
-
     public interface SetWeight{
-        ExtraHeightWithBuild setWeight(String weight);
+        Build setWeight(String weight);
     }
 
-    public interface Extra extends SetHeight, SetWeight,Build{
-
-    }
-    public interface ExtraHeightWithBuild extends SetHeight,Build{
-
-    }
-    public interface ExtraWeightWithBuild extends SetWeight,Build{
-
-    }
     public interface Build {
         User build();
     }
 
-    public static class Builder implements SetName, SetAge, Extra, ExtraHeightWithBuild, ExtraWeightWithBuild{
-        private String name = "Mr Anex";
-        private String age = "79";
-        private String height = "5\'8\"";
-        private String weight = "78";
-
-        @Override
-        public SetAge setName(String name) {
-            this.name = name;
-            return this;
-        }
-
-        @Override
-        public Extra setAge(String age) {
-            this.age = age;
-            return this;
-        }
-
-        @Override
-        public Builder setHeight(String height) {
-            this.height = height;
-            return this;
-        }
-
-        @Override
-        public ExtraHeightWithBuild setWeight(String weight) {
-            this.weight = weight;
-            return this;
-        }
-
-        @Override
-        public User build() {
-            return new User(name, age, height, weight);
+    public static class Builder {
+        private SetName setName = (name1 -> (age1 -> (height1 -> (weight1 -> ()-> new User(name1,age1,height1,weight1)))));
+        SetName getSetName() {
+            return setName;
         }
     }
 }
